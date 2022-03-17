7a0fc35a59cdacea1c44e6b002c95d0bf5d4a2d5
mutuajoseph <wambuajoseph756@gmail.com>
2022-03-17 13:42:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+let employee = {
+	name: "Joseph Wambua",
+	streetAddress: 678906
+}
+
+//this function updates the object without changing the original object 
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newObj = { ...employee };
+
+  newObj[key] = value;
+
+  return newObj;
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+  employee,
+  "name",
+  "Sam"
+);
+
+//updates the object but returns a completely new object 
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+     employee[key] = value;
+     return employee;
+}
+
+//deleting employee using key without changing original object
+function deleteFromEmployeeByKey(employee,key){
+	//making a clone of the object
+  const newObj = {...employee};
+  delete newObj[key];
+  return newObj;
+
+}
+//deleting employee using key and changing original object 
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+	delete employee[key];
+	return employee;
+}
